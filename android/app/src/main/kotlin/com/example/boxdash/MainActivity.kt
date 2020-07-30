0b4a463594454108aab0049ef2ede327a0a17191
Abhay Maurya <maurya.abhay30@gmail.com>
2020-07-31 01:23:20 +0530

    init

---
+++ android/app/src/main/kotlin/com/example/boxdash/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.boxdash
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flame/util.dart';
+import 'package:flutter/services.dart';
+
+void main() async {
+  Util flameUtil = Util();
+  await flameUtil.fullScreen();
+  await flameUtil.setOrientation(DeviceOrientation.portraitUp);
+}
